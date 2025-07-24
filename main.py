9266fbc91bc23a2e0ed307cdb85b3f5f3ffa410d
Stanislav Nemirovsky <choonsky@gmail.com>
2025-07-24 15:14:35 +0600

    Python DB connections

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-# This is a sample Python script.
+import oracledb
+import psycopg2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+try:
+    # Establish a connection with Oracle
+    connectionOracle = oracledb.connect(
+        user="IN_DATA",
+        password="12345",
+        dsn="localhost:1521/FREEPDB1"  # Replace with your connection details
+    )
+    print("Successfully connected to Oracle!")
 
+    connectionPostgres = psycopg2.connect(
+        dbname='postgres',
+        user='postgres',
+        password='admin',
+        host='localhost')
+    print("Successfully connected to PostgreSQL!")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Create an Oracle cursor object
+    cursorOracle = connectionOracle.cursor()
 
+    # Execute a query
+    cursorOracle.execute("SELECT count(*) FROM in_data.data_main")  # Replace with your table name
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Fetch and print the results
+    for row in cursorOracle:
+        print(row)
+
+    # Close the cursor and connection
+    cursorOracle.close()
+    connectionOracle.close()
+
+except oracledb.Error as error:
+    print(f"Oracle Error: {error}")
+except psycopg2.Error as error:
+    print(f"Postgres Error: {error}")
+except Exception as error:
+    print(f"An error occurred: {error}")
