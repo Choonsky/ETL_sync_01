1e9f11b473911ed995cccf9f09ddc41bc1fecfd3
Stanislav Nemirovsky <choonsky@gmail.com>
2025-07-24 14:57:34 +0600

    Postgres files

--- sql/Postgres_init_ddl.sql
+++ sql/Postgres_init_ddl.sql
@@ -1,18 +1,16 @@
-CREATE TABLE result_data
+CREATE TABLE IF NOT EXISTS public.result_data
 (
-    -- Creating new UUIDs because there can be many versions
-    id                      UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    -- Old fields
-    id_indata               UUID,
-    date_created            DATE   not null,
-    date_updated            DATE,
-    user_created            UUID   not null,
-    user_updated            UUID,
-    transaction_id          UUID   not null,
-    transaction_status_new  VARCHAR2(8) not null,
-    transaction_amount      NUMBER not null,
-    transaction_currency    VARCHAR2(3) not null,
-    transaction_description NVARCHAR2(200) not null,
-    -- New field
-    version                 NUMBER not null;
-);
+    id uuid NOT NULL,
+    id_indata uuid NOT NULL,
+    date_created date NOT NULL,
+    date_updated date,
+    user_created uuid NOT NULL,
+    user_updated uuid,
+    transaction_id uuid NOT NULL,
+    transaction_status_new character varying(8) COLLATE pg_catalog."default" NOT NULL,
+    transaction_amount numeric NOT NULL,
+    transaction_currency character(3) COLLATE pg_catalog."default" NOT NULL,
+    transaction_description character varying(200) COLLATE pg_catalog."default",
+    version smallint NOT NULL,
+    CONSTRAINT result_data_pkey PRIMARY KEY (id)
+)
